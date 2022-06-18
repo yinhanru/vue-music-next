63a22b6a2c6df413053734799195b0ec9e2cc285
yinhanru <yinhanru@corp.netease.com>
2022-06-18 16:38:22 +0800

    feat: vue3脚手架

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <m-header></m-header>
+</template>
+
+<script>
+import MHeader from '@/components/header/header'
+export default {
+  components: { MHeader }
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        //  全局引入变量和mixin
+        additionalData: `
+          @import "@/assets/scss/variable.scss";
+          @import "@/assets/scss/mixin.scss";
+        `
+      }
+    }
+  }
+})
